f22ff3f014945e6cefe32d60b84dffa2782430bb
cloudadvocate <59128159+cloudadvocate@users.noreply.github.com>
2020-09-06 22:03:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def hello_world(request):
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
-        return f'Hello World!'
+        return f'Hello World!!!'
