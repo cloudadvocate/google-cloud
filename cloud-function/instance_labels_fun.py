67095e19270b88c22dbeedf199d39625a5578ce1
hemantakhandare <42443539+hemantakhandare@users.noreply.github.com>
2022-02-25 00:05:04 +0530

    Update instance_labels_fun.py

--- cloud-function/instance_labels_fun.py
+++ cloud-function/instance_labels_fun.py
@@ -1,26 +1,26 @@
 import base64
 import json
 import requests
-from googleapiclient import discovery
 from pprint import pprint
+from googleapiclient import discovery
 
 
+def get_labelFingerprint(response):
+    return (response["labelFingerprint"])
 
-def hello_pubsub(event, context):
-    """Triggered from a message on a Cloud Pub/Sub topic.
-    Args:
-         event (dict): Event payload.
-         context (google.cloud.functions.Context): Metadata for the event.
-    """
+def set_label_instance(event, context):
+    
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    #print(pubsub_message)
     final = json.loads(pubsub_message)
-    resource_name = final['jsonPayload']['resource']['name']
+    resource_name = final['protoPayload']['request']['name']
     project_id = final['resource']['labels']['project_id']
     zone = final['resource']['labels']['zone']
     
-    
-    #print(pubsub_message)
     print(resource_name)
+    print(project_id)
+    print(zone)
+    
     
     service = discovery.build('compute', 'v1')  
     # Project ID for this request.
@@ -30,18 +30,30 @@ def hello_pubsub(event, context):
     zone = zone  # TODO: Update placeholder value.
 
     # Name of the instance scoping this request.
-    instance = resource_name  # TODO: Update placeholder value.
+    instance = resource_name  # TODO: Update placeholder value.   
+    
+#main code starts here:create instance 
+    service = discovery.build('compute', 'v1')
+
+    request = service.instances().get(project=project, zone=zone, instance=instance)
+    response = request.execute()
+    
+#call to get_labelFingerprint() to get labelFingerprint value
+    labelFingerprint=get_labelFingerprint(response)
+    print("labelFingerprint:-->"+labelFingerprint)
 
     instances_set_labels_request_body = {
     # TODO: Add desired entries to the request body.
         "labels": {
-          "environment": "test"
+          "environment": "test_environment"
          },
-        "labelFingerprint": "42WmSpB8rSM="
+       'labelFingerprint': labelFingerprint
     }
 
     request = service.instances().setLabels(project=project, zone=zone, instance=instance, body=instances_set_labels_request_body)
     response = request.execute()
 
     # TODO: Change code below to process the `response` dict:
-    pprint(response)
+    #pprint(response)
+
+
